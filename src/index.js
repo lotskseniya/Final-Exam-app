d455afc9da754590ea3038c842ccc43b37492d02
lotskseniya <kseniyalotsman@gmail.com>
2023-07-17 23:44:46 +0300

    Exam has been prepared

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { App } from './App.jsx';
+import { Map } from './components/Map/Map';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Settings } from './components/Settings/Settings';
+import { Cities } from './components/Cities/Cities';
+
+
+const router = createBrowserRouter([
+  { path: "/", element: <App/> },
+  { path: "/cities", element: <Cities/> },
+  { path: "/map", element: <Map/> },
+  { path: "/settings", element: <Settings/> },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <RouterProvider router={router}>
     <App />
+    </RouterProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
